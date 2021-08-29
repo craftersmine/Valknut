e85d3873ed2576a0f0eb2f0c0a6431eddf703a5f
craftersmine <rodion.morozov.01@gmail.com>
2021-08-29 16:25:42 +0700

    Added Bootstrap settings

---
+++ craftersmine.Valknut.Launcher.Bootstrap/BootstrapSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.Valknut.Launcher.Bootstrap
+{
+    public static class BootstrapSettings
+    {
+        public static string ServerAddress { get; } = "localhost";
+        public static string ServerProtocol { get; } = "http";
+        public static int ServerPort { get; } = 80;
+    }
+}
