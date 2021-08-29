1fa710d85fc7f93521219967c678b384f02f6b2f
craftersmine <rodion.morozov.01@gmail.com>
2021-08-29 16:25:09 +0700

    Added Bootstrap Data class

---
+++ craftersmine.Valknut.Launcher.Bootstrap/BootstrapData.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craftersmine.Valknut.Launcher.Bootstrap
+{
+    public sealed class BootstrapData
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("version")]
+        public int Version { get; set; }
+        [JsonProperty("versionString")]
+        public string VersionString { get; set; }
+        [JsonProperty("hash", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public string Hash { get; set; }
+        [JsonProperty("archive")]
+        public string Archive { get; set; }
+    }
+}
