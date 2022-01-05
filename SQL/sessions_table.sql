f2a0eeaf51e50dd64d3497522ed2b1843d35873d
craftersmine <rodion.morozov.01@gmail.com>
2022-01-05 21:02:57 +0700

    Fixed SQL query

--- SQL/sessions_table.sql
+++ SQL/sessions_table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `sessions` (
   `uuid` varchar(36) NOT NULL COMMENT 'User Unique ID identical to uuid from accounts table',
-  `username` varchar(32) DEFAULT NULL 'User username identical to username from accounts table',
-  `serverId` varchar(48) DEFAULT NULL 'User last server ID',
-  `sessionId` varchar(255) DEFAULT NULL 'User current session ID',
-  `clientToken` varchar(36) DEFAULT NULL 'User current client token',
+  `username` varchar(32) DEFAULT NULL COMMENT 'User username identical to username from accounts table',
+  `serverId` varchar(48) DEFAULT NULL COMMENT 'User last server ID',
+  `sessionId` varchar(255) DEFAULT NULL COMMENT 'User current session ID',
+  `clientToken` varchar(36) DEFAULT NULL COMMENT 'User current client token',
   PRIMARY KEY (`uuid`),
   UNIQUE KEY `uuid_UNIQUE` (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
