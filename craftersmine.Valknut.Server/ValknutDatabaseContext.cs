2b4d7a8c03eaf256be66f012322fe68e1ef3ae77
craftersmine <rodion.morozov.01@gmail.com>
2024-02-20 01:34:26 +0700

    Added DB context scaffold

---
+++ craftersmine.Valknut.Server/ValknutDatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MySql.EntityFrameworkCore.Infrastructure;
+
+namespace craftersmine.Valknut.Server
+{
+    public class ValknutDatabaseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL();
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
