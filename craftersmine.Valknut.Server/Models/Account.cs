f54b9908d6c02d3a3dab5feccfbcfe312650fb8e
craftersmine <rodion.morozov.01@gmail.com>
2024-02-20 01:35:00 +0700

    Added DB models

---
+++ craftersmine.Valknut.Server/Models/Account.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace craftersmine.Valknut.Server.Models
+{
+    public class Account
+    {
+        [NotMapped]
+        public Guid Uuid
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(UuidDb))
+                    return Guid.Empty;
+
+                return Guid.Parse(UuidDb);
+            }
+        }
+        [Column("uuid")]
+        public string UuidDb { get; set; }
+        [Column("username")]
+        public string Username { get; set; }
+        [Column("email")]
+        public string Email { get; set; }
+        [Column("password")]
+        public string EncryptedPassword { get; set; }
+    }
+}
---
+++ craftersmine.Valknut.Server/Models/UserSession.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Guid = System.Guid;
+
+namespace craftersmine.Valknut.Server.Models
+{
+    public class UserSession
+    {
+        [NotMapped]
+        public Guid ClientToken
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(ClientTokenDb))
+                    return Guid.Empty;
+
+                return Guid.Parse(ClientTokenDb);
+            }
+        }
+
+        [NotMapped]
+        public Guid Uuid
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(UuidDb))
+                    return Guid.Empty;
+
+                return Guid.Parse(UuidDb);
+            }
+        }
+        
+        [Column("clientToken")]
+        public string ClientTokenDb { get; set; }
+        [Column("sessionId")]
+        public string SessionId { get; set; }
+        [Column("serverId")]
+        public string ServerId { get; set; }
+        [Column("uuid")]
+        public string UuidDb { get; set; }
+    }
+}
