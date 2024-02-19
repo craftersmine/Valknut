cace03454a07544ca1c42ee8515985ce25ba4f31
craftersmine <rodion.morozov.01@gmail.com>
2024-02-20 01:34:42 +0700

    Added authentication controller scaffold

---
+++ craftersmine.Valknut.Server/Controllers/AuthenticationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace craftersmine.Valknut.Server.Controllers
+{
+    [Route("valknut/v2/auth")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpPost("authenticate")]
+        public async Task<ActionResult> Authenticate()
+        {
+            return Ok(null);
+        }
+    }
+}
